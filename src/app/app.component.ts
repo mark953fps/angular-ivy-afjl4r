5c818f82e41dcc56cc259c18139bfd7482f11844
mark953fps <markville953@gmail.com>
2021-07-23 10:58:41 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public groupDataList: any = [
+    { groupName: 'test1', groupId: '1' },
+    { groupName: 'test2', groupId: '2' },
+    { groupName: 'test3', groupId: '3' },
+    { groupName: 'test4', groupId: '4' },
+    { groupName: 'test5', groupId: '5' },
+    { groupName: 'test6', groupId: '6' },
+    { groupName: 'test7', groupId: '7' },
+    { groupName: 'test8', groupId: '8' },
+    { groupName: 'test9', groupId: '9' },
+    { groupName: 'test10', groupId: '10' },
+    { groupName: 'test11', groupId: '11' },
+    { groupName: 'test12', groupId: '12' },
+    { groupName: 'test13', groupId: '13' },
+    { groupName: 'test14', groupId: '14' },
+    { groupName: 'test15', groupId: '15' },
+    { groupName: 'test16', groupId: '16' },
+    { groupName: 'test17', groupId: '17' }
+  ];
+
+  public groupDataListChunkArr = [];
+
+  public ngOnInit() {
+    this.groupDataListChunkArr = this.sliceIntoChunks(this.groupDataList, 5);
+    console.log(this.sliceIntoChunks(this.groupDataList, 5));
+  }
+
+  public sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      const chunk = arr.slice(i, i + chunkSize);
+      res.push(chunk);
+    }
+    return res;
+  }
 }
